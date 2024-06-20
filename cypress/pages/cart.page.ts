428190bd774d4e213404fd4eb210bf3685155588
Divya Arora <134497708+divyaarora23@users.noreply.github.com>
2024-06-20 18:16:04 +0530

    Changes
    Changes

--- cypress/pages/cart.page.ts
+++ cypress/pages/cart.page.ts
@@ -2,11 +2,13 @@ export class productPage{
     
     static add()
     {
-        cy.get('')
+        cy.get('.inventory_item').click();
     }
     static remove()
     {
-        cy.get('')
+        cy.get('.shopping_cart_link').click();
+        cy.get('.cart_item').first().find('.cart_button').click();
+        cy.get('.shopping_cart_badge').should('not.exist');
     }
     
 }
